d19b4001a61ce6c75f180d879e688fbeba620189
maudp <mauduong.dev@gmail.com>
2023-07-16 23:26:48 +0700

    feat: add auth init

--- apps/trello/src/app/app.tsx
+++ apps/trello/src/app/app.tsx
@@ -1,14 +1,31 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
+import {
+  Route,
+  Link,
+  createBrowserRouter,
+  RouterProvider,
+  useRoutes,
+} from 'react-router-dom';
+import { useEffect } from 'react';
+import { HomeFeature } from '@trello/home/feature';
+import { AppInit } from '@trello/common/app-init';
 
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="trello" />
-    </div>
-  );
+  const routes = useRoutes([
+    {
+      path: '/',
+      element: <HomeFeature />,
+    },
+  ]);
+
+  useEffect(() => {
+    const onInit = async () => {
+      await AppInit.initApp();
+    };
+
+    onInit();
+  }, []);
+
+  return routes;
 }
 
 export default App;
