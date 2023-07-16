8ee6e87c38873f573b47ed9f2785cb6427de6094
maudp <mauduong.dev@gmail.com>
2023-07-17 00:08:00 +0700

    feat: update init auth interceptor

--- libs/common/app-init/src/lib/app-init.ts
+++ libs/common/app-init/src/lib/app-init.ts
@@ -1,15 +1,16 @@
 import { keycloak } from '@trello/auth/data-access';
+import { AuthInterceptor } from '@trello/auth/util';
 
 export class AppInit {
   static async initApp() {
     await this.initAuth();
+    AuthInterceptor.onInit();
   }
 
   private static async initAuth() {
     if (keycloak.authenticated) {
       return;
     }
-
     await keycloak.init({
       onLoad: 'login-required',
       pkceMethod: 'S256',
